3471cdb5b8b357e27f04e018e817366df3e18a09
daniel2024 <daniehuancapiza@frba.utn.edu.ar>
2021-12-12 18:03:31 -0300

    add changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c84d9a8a967c27c3"; }
+/******/ 		__webpack_require__.h = function() { return "abcf58d2e8e8e571"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Card/ContainerCard.tsx
+++ components/Card/ContainerCard.tsx
@@ -1,34 +1,44 @@
-import React from 'react'
+import React, { FC } from 'react'
 import { Card, Icon, Image } from 'semantic-ui-react'
 import { CartItemType } from 'Store/StoreCard'
 
-type ProductListProps = {
+export interface TProduct {
+    attributes: any
+    id: string
+    image: string
+    name: string
+    price: number
+    sku: string
+}
+
+
+interface ContainerCardProps {
     products: TProduct[]
 }
-  
 
-const ContainerCard = (products: TProduct[]) => {
+
+const ContainerCard: FC<ContainerCardProps> = ({ products }) => {
 
     return (
         <>
-        <Card>
-            <Image src={products[0].image} wrapped ui={false} />
-            <Card.Content>
-            <Card.Header>Matthew</Card.Header>
-            <Card.Meta>
-                <span className='date'>Joined in 2015</span>
-            </Card.Meta>
-            <Card.Description>
-            {products[0].name}
-            </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-            <a>
-                <Icon name='user' />
-                {products[0].price}
-            </a>
-            </Card.Content>
-        </Card>
+            <Card>
+                <Image src={products[0]?.image} wrapped ui={false} />
+                <Card.Content>
+                    <Card.Header>Matthew</Card.Header>
+                    <Card.Meta>
+                        <span className='date'>Joined in 2015</span>
+                    </Card.Meta>
+                    <Card.Description>
+                        {products[0]?.name}
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <a>
+                        <Icon name='user' />
+                        {products[0]?.price}
+                    </a>
+                </Card.Content>
+            </Card>
         </>
     )
 }
